e68a21d4e2d59235206a6ae0b9585725ef7a5c61
Volha_Manko <volha.manko@workhuman.com>
2023-02-25 20:55:36 +0100

    [Task-3] Added error handlers

--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,15 +1,35 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-import { productsList } from '@functions/getProductsList/productsList';
+import { getProductsListMock } from '@functions/getProductsList/getProductsListMock';
 import schema from './schema';
+import { StatusCodes } from '../../constants/statusCodes';
 
 const getProductById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  const productId = event.pathParameters?.productId;
+  try {
+    const productsList = await getProductsListMock();
+    const productId = event.pathParameters?.productId;
+    const product = productsList.find((product) => product.id === productId);
   
-  const product = productsList.find((product) => product.id === productId);
-  
-  return formatJSONResponse(product);
+    if (product) {
+      return formatJSONResponse(product);
+    }
+    
+    return formatJSONResponse(
+        {
+          message: 'Product not found'
+        },
+        StatusCodes.NotFound
+    );
+    
+  } catch (e) {
+    return formatJSONResponse(
+        {
+          message: 'Error while fetching product'
+        },
+        StatusCodes.InternalServerError
+    );
+  }
 };
 
 export const main = middyfy(getProductById);
--- product-service/src/libs/api-gateway.ts
+++ product-service/src/libs/api-gateway.ts
@@ -1,12 +1,13 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
 import type { FromSchema } from "json-schema-to-ts";
+import { StatusCodes } from '../constants/statusCodes';
 
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
-export const formatJSONResponse = (response: Record<string, unknown> | Array<Record<string, unknown>>) => {
+export const formatJSONResponse = (response: Record<string, unknown> | Array<Record<string, unknown>>, statusCode?: number) => {
   return {
-    statusCode: 200,
+    statusCode: statusCode || StatusCodes.Success,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
