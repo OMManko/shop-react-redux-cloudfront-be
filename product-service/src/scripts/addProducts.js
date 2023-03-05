5b8bb16ded5f6710c8bdf361c2c7d4eb92ace1b1
Volha_Manko <volha.manko@workhuman.com>
2023-03-05 15:25:15 +0100

    [Task-4] Added script for uploading products to the table

--- product-service/src/scripts/addProducts.js
+++ product-service/src/scripts/addProducts.js
@@ -1,79 +1,21 @@
 const AWS = require('aws-sdk');
+const fs = require('fs');
 const REGION = 'eu-central-1';
 AWS.config.update({ region: REGION });
 
 const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+const products = fs.readFileSync(require.resolve("./productsList.json"));
+const productsRequests = JSON.parse(products.toString()).map((product) => ({
+    PutRequest: {
+        Item: {
+            ...product,
+        },
+    },
+}));
 
 const params = {
     RequestItems: {
-        "product-service-products-list-table-dev": [
-            {
-                PutRequest: {
-                    Item: {
-                        "id": { "S": "7567ec4b-b10c-48c5-9345-fc73c48a80aa" },
-                        "title": { "S": "Eiffel tower0" },
-                        "description": { "S": "Build a detailed model of the Eiffel Tower in Paris, France!" },
-                        "count": { "N": "24" },
-                        "price": { "N": "110" }
-                    }
-                }
-            },
-            {
-                PutRequest: {
-                    Item: {
-                        "id": { "S": "7567ec4b-b10c-48c5-9345-fc73c48a80a1" },
-                        "title": { "S": "LEGO® Titanic" },
-                        "description": { "S": "Build the ship of dreams with the new LEGO® Titanic" },
-                        "count": { "N": "20" },
-                        "price": { "N": "100" }
-                    }
-                }
-            },
-            {
-                PutRequest: {
-                    Item: {
-                        "id": { "S": "7567ec4b-b10c-48c5-9345-fc73c48a80a3" },
-                        "title": { "S": "Diagon Alley™" },
-                        "description": { "S": "Enter the world of Diagon Alley™" },
-                        "count": { "N": "25" },
-                        "price": { "N": "90" }
-                    }
-                }
-            },
-            {
-                PutRequest: {
-                    Item: {
-                        "id": { "S": "7567ec4b-b10c-48c5-9345-fc73348a80a1" },
-                        "title": { "S": "Lion Knights' Castle" },
-                        "description": { "S": "Go medieval with an amazing new set inspired by legendary LEGO® castles" },
-                        "count": { "N": "35" },
-                        "price": { "N": "86" }
-                    }
-                }
-            },
-            {
-                PutRequest: {
-                    Item: {
-                        "id": { "S": "7567ec4b-b10c-48c5-9445-fc73c48a80a2" },
-                        "title": { "S": "Jazz Club" },
-                        "description": { "S": "Get your ticket to the greatest show around" },
-                        "count": { "N": "5" },
-                        "price": { "N": "120" }
-                    }
-                }
-            },
-            {
-                PutRequest: {
-                    Item: {
-                        "id": { "S": "7567ec4b-b10c-45c5-9345-fc73c48a80a1" },
-                        "title": { "S": "Santa's Sleigh" },
-                        "description": { "S": "Build a flying sleigh with Aira Windwhistler, the Wind Elf!" },
-                        "count": { "N": "25" },
-                        "price": { "N": "20" }
-                    }
-                }
-            }
-        ]
+        "product-service-products-list-table-dev": productsRequests
     }
 };
 
